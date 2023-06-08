0beaef9df2a202e0ff7c4c9fed14aac90d83be8e
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-06-08 06:26:27 -0700

    created some notes to start my challenge

---
+++ index.js
@@ -0,0 +1,7 @@
+/** Each shape should pass through a code as instructed in the readme file */
+
+//Create the function for a triangle shape 
+
+//Create the function for a cirlce shape
+
+// Create the function for a square shape 
